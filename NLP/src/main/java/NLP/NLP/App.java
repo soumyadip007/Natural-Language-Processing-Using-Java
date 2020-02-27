0e43b602cf17f898fe460ed69cb6b0e6668ad5cb
soumyadip007 <soumyadip.note@GMAIL.COM>
2020-02-27 23:40:15 +0530

    NLP Module Framework Added

---
+++ NLP/src/main/java/NLP/NLP/App.java
@@ -0,0 +1,13 @@
+package NLP.NLP;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
