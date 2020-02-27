32eb23bc99388e5f74bc3f9ca13f9c3534c4915b
soumyadip007 <soumyadip.note@GMAIL.COM>
2020-02-27 23:53:16 +0530

    Fields Added and Pipline removed so object cannot be created

--- NLP/src/main/java/com/nlp/Pipeline.java
+++ NLP/src/main/java/com/nlp/Pipeline.java
@@ -1,5 +1,17 @@
 package com.nlp;
 
+import java.util.Properties;
+
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+
 public class Pipeline {
 
+	private static Properties properties;
+	private static String propertyname="";
+	private static StanfordCoreNLP sanfordCoreNLP; 
+	
+	private Pipeline()
+	{
+		
+	}
 }
