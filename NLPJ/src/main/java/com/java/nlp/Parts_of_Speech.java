d370f11814a44c3c2939293b07280421266313c0
soumyadip007 <soumyadip.note@GMAIL.COM>
2020-02-28 22:47:12 +0530

    POS Main added

--- NLPJ/src/main/java/com/java/nlp/Parts_of_Speech.java
+++ NLPJ/src/main/java/com/java/nlp/Parts_of_Speech.java
@@ -1,5 +1,17 @@
 package com.java.nlp;
 
+import java.util.List;
+
+import com.java.nlp.main.Pipeline;
+
+import edu.stanford.nlp.pipeline.CoreDocument;
+import edu.stanford.nlp.pipeline.CoreSentence;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+
 public class Parts_of_Speech {
 
+
+	public static void main(String args[])
+	{
+	}
 }
