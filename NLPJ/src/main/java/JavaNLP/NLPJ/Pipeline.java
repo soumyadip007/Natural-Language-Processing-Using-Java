88d226eff82ddb96d9a3d65ad0074bf4d0731ab2
soumyadip007 <soumyadip.note@GMAIL.COM>
2020-02-28 22:22:10 +0530

    NLP Commit

---
+++ NLPJ/src/main/java/JavaNLP/NLPJ/Pipeline.java
@@ -0,0 +1,35 @@
+package JavaNLP.NLPJ;
+
+
+import java.util.Properties;
+
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+
+public class Pipeline {
+
+	private static Properties properties;
+	private static String propertyname="";
+	private static StanfordCoreNLP sanfordCoreNLP; 
+	
+	private Pipeline()
+	{
+		//hiding object
+	}
+	
+	public static StanfordCoreNLP getPipeline()
+	{
+		
+		if(sanfordCoreNLP==null)
+			sanfordCoreNLP=new StanfordCoreNLP(properties);
+		
+		return sanfordCoreNLP;
+	}
+	
+	static {
+		
+		properties=new Properties();
+		properties.setProperty("annotator", propertyname);
+		
+	}
+	
+}
---
+++ NLPJ/src/main/java/JavaNLP/NLPJ/Tokenize.java
@@ -0,0 +1,14 @@
+package JavaNLP.NLPJ;
+
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+
+public class Tokenize {
+
+	public static void main(String args[])
+	{
+		StanfordCoreNLP sanfordCoreNLP=Pipeline.getPipeline();
+		
+		String text
+		
+	}
+}
