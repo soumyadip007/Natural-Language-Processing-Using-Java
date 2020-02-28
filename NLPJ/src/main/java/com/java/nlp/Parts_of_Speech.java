fe50b173d31d42d8d3712e4d1cb5ef8862ecf5d7
soumyadip007 <soumyadip.note@GMAIL.COM>
2020-02-28 22:52:24 +0530

    POS Added

--- NLPJ/src/main/java/com/java/nlp/Parts_of_Speech.java
+++ NLPJ/src/main/java/com/java/nlp/Parts_of_Speech.java
@@ -2,8 +2,12 @@ package com.java.nlp;
 
 import java.util.List;
 
+import org.omg.CORBA.SystemException;
+
 import com.java.nlp.main.Pipeline;
 
+import edu.stanford.nlp.coref.CorefCoreAnnotations.CorefAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations;
 import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.pipeline.CoreDocument;
 import edu.stanford.nlp.pipeline.CoreSentence;
@@ -26,6 +30,10 @@ public class Parts_of_Speech {
 		List<CoreLabel> res=core.tokens();
 		
 		for(CoreLabel c:res)
-			System.out.println(c);
+		{
+			String pos=c.get(CoreAnnotations.PartOfSpeechAnnotation.class);
+			System.out.println(c.originalText()+" = "+pos);
+		}
+		
 	}
 }
