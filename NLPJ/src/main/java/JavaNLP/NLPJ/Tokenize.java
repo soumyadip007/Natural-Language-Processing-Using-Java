d3b19ec550769171c640c2a03efb6c71ab2700c9
soumyadip007 <soumyadip.note@GMAIL.COM>
2020-02-28 22:25:00 +0530

    CoreSentence

--- NLPJ/src/main/java/JavaNLP/NLPJ/Tokenize.java
+++ NLPJ/src/main/java/JavaNLP/NLPJ/Tokenize.java
@@ -1,5 +1,9 @@
 package JavaNLP.NLPJ;
 
+import java.util.List;
+
+import edu.stanford.nlp.pipeline.CoreDocument;
+import edu.stanford.nlp.pipeline.CoreSentence;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
 
 public class Tokenize {
@@ -8,7 +12,14 @@ public class Tokenize {
 	{
 		StanfordCoreNLP sanfordCoreNLP=Pipeline.getPipeline();
 		
-		String text
+		String text="Hey! This is soumyadip";
+		
+		CoreDocument core=new CoreDocument(text);
+		
+		sanfordCoreNLP.annotate(core);
+		
+		List<CoreSentence> res=core.sentences();
+		
 		
 	}
 }
